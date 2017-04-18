b10caad315ab4b018077364d634114affa6e07a4
Ian Lamb <ianlamb32@gmail.com>
2017-04-17 21:23:46 -0700

    WIP

--- public/app.js
+++ public/app.js
@@ -6,3 +6,22 @@ setTimeout(function() {
 setTimeout(function() {
     dividers[1].classList.add('active');
 }, 2000);
+
+
+// reset hash on page refresh
+window.location.hash = 'wedding';
+
+function getHash(url) {
+    var tokens = url.split('#');
+    return tokens.length > 1 ? tokens[1] : null;
+}
+
+window.addEventListener('hashchange', function(e) {
+    var oldHash = getHash(e.oldURL);
+    var newHash = getHash(e.newURL);
+    var oldContent = document.querySelector('.timeline-section.active');
+    var newContent = document.getElementById(newHash);
+    
+    oldContent.classList.remove('active');
+    newContent.classList.add('active');
+});
