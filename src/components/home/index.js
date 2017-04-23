152330688e6d69d8ea1c4970f50db5874ac32b93
Ian Lamb <ianlamb32@gmail.com>
2017-04-22 20:34:53 -0700

    fix functionality breakages

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,27 +5,33 @@ export default class Home extends Component {
 	constructor() {
         super();
 
-        this.timeline = {
+		this.handleNavClick = this.handleNavClick.bind(this);
+
+        this.state = {
 			sections: [
 				{
 					id: 'first-date',
 					title: 'The Couple',
-					description: 'Bacon ipsum dolor amet fatback tail pancetta bresaola tenderloin pig. Pork loin andouille ham hock venison rump, spare ribs bresaola sausage shank ribeye tri-tip pastrami pork kielbasa.'
+					description: 'Bacon ipsum dolor amet fatback tail pancetta bresaola tenderloin pig. Pork loin andouille ham hock venison rump, spare ribs bresaola sausage shank ribeye tri-tip pastrami pork kielbasa.',
+					active: false
 				},
 				{
 					id: 'proposal',
 					title: 'The Proposal',
-					description: 'Strip steak corned beef leberkas drumstick jerky tri-tip ham salami burgdoggen chuck. Kielbasa ham turkey bacon pork loin landjaeger t-bone.'
+					description: 'Strip steak corned beef leberkas drumstick jerky tri-tip ham salami burgdoggen chuck. Kielbasa ham turkey bacon pork loin landjaeger t-bone.',
+					active: false
 				},
 				{
 					id: 'wedding',
 					title: 'The Wedding',
-					description: 'Biltong chuck ground round, pastrami swine alcatra meatball. Cow sausage pork chop, sirloin bresaola leberkas swine picanha boudin pork t-bone.'
+					description: 'Biltong chuck ground round, pastrami swine alcatra meatball. Cow sausage pork chop, sirloin bresaola leberkas swine picanha boudin pork t-bone.',
+					active: true
 				},
 				{
 					id: 'after-party',
 					title: 'The After Party',
-					description: 'Pork loin andouille ham hock venison rump, spare ribs bresaola sausage shank ribeye tri-tip pastrami pork kielbasa.'
+					description: 'Pork loin andouille ham hock venison rump, spare ribs bresaola sausage shank ribeye tri-tip pastrami pork kielbasa.',
+					active: false
 				}
 			],
 			navItems: [
@@ -53,41 +59,47 @@ export default class Home extends Component {
 		};
     }
 
-	componentDidUpdate() {
+	componentDidMount() {
 		var dividers = document.querySelectorAll('.' + style.navDivider);
 
 		setTimeout(function() {
 			dividers[0].classList.add(style.complete);
-		}, 500);
+		}, 200);
 		setTimeout(function() {
 			dividers[1].classList.add(style.active);
-		}, 2000);
+		}, 1000);
+		
+		// 	// reset hash on page refresh
+		// 	window.location.hash = 'wedding';
 
+		// 	function getHash(url) {
+		// 		var tokens = url.split('#');
+		// 		return tokens.length > 1 ? tokens[1] : null;
+		// 	}
 
-		// reset hash on page refresh
-		window.location.hash = 'wedding';
+		// 	window.addEventListener('hashchange', function(e) {
+		// 		var oldHash = getHash(e.oldURL);
+		// 		var newHash = getHash(e.newURL);
+		// 		var oldContent = document.querySelector('.' + style.section + '.' + style.active);
+		// 		var newContent = document.getElementById(newHash);
+				
+		// 		oldContent.classList.remove(style.active);
+		// 		newContent.classList.add(style.active);
+		// 	});
+	}
 
-		function getHash(url) {
-			var tokens = url.split('#');
-			return tokens.length > 1 ? tokens[1] : null;
-		}
-
-		window.addEventListener('hashchange', function(e) {
-			var oldHash = getHash(e.oldURL);
-			var newHash = getHash(e.newURL);
-			var oldContent = document.querySelector('.' + style.section + '.' + style.active);
-			var newContent = document.getElementById(newHash);
-			
-			oldContent.classList.remove(style.active);
-			newContent.classList.add(style.active);
+	handleNavClick(e) {
+		let sectionId = e.currentTarget.href.split('#')[1];
+		this.state.sections.forEach((section) => {
+			section.active = (sectionId === section.id);
 		});
-
 	}
 
 	render() {
-		let sectionList = this.timeline.sections.map((section) => {
+		let sectionList = this.state.sections.map((section) => {
+			let sectionClasses = style.section + ' ' + (section.active ? style.active : '');
 			return (
-				<section class={style.section} id={section.id}>
+				<section class={sectionClasses} id={section.id}>
 					<div class={style.content}>
 						<h2>{section.title}</h2>
 						<p>{section.description}</p>
@@ -96,14 +108,13 @@ export default class Home extends Component {
 			)
 		})
 
-		let navItems = this.timeline.navItems.map((navItem) => {
+		let navItems = this.state.navItems.map((navItem) => {
 			return (
-				<div class={style.navItemWrapper}>
-					<div class={style.navItem}>
-						<a href={navItem.href} class={style.navLink}></a>
+				<div class={style.navItem}>
+					<a href={navItem.href} class={style.navLink} onClick={this.handleNavClick}>
 						<div class={style.navTitle}>{navItem.title}</div>
 						<div class={style.navDate} dangerouslySetInnerHTML={{__html: navItem.date}} />
-					</div>
+					</a>
 
 					<div class={style.navDivider}></div>
 				</div>
