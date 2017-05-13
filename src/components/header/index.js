a0ecc2dbba19a806c3e1a5d21f9077e6c485d88e
Ian Lamb <ianlamb32@gmail.com>
2017-05-12 23:51:30 -0700

    implement basic rsvp system

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,39 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router';
+import { RsvpMask, RsvpModal } from '../rsvp';
 import style from './style.less';
 
 export default class Header extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			showRsvpMask: false,
+			showRsvpModal: false
+		}
+
+		this.onRsvpBtnClick = this.onRsvpBtnClick.bind(this);
+		this.onRsvpMaskClick = this.onRsvpMaskClick.bind(this);
+	}
+
+	onRsvpBtnClick() {
+		this.setState({ showRsvpMask: true, showRsvpModal: true });
+	}
+
+	onRsvpMaskClick() {
+		this.setState({ showRsvpMask: false, showRsvpModal: false });
+	}
+
 	render() {
 		return (
 			<header class={style.header}>
 				<div class={style.date}>December 23rd, 2017</div>
 				<div class={style.title}>KAT &amp; IAN</div>
 				<div class={style.rsvp}>
-					<button class={style.rsvpBtn}>RSVP</button>
+					<button class={style.rsvpBtn} onClick={this.onRsvpBtnClick}>RSVP</button>
 				</div>
+				<RsvpMask show={this.state.showRsvpMask} onClick={this.onRsvpMaskClick} />
+				<RsvpModal show={this.state.showRsvpModal} />
 			</header>
 		);
 	}
---
+++ src/components/rsvp/index.js
@@ -0,0 +1,105 @@
+import { h, Component } from 'preact';
+import style from './style.less';
+
+export class RsvpModal extends Component {
+	constructor() {
+        super();
+
+        this.state = {};
+
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onInputChange = this.onInputChange.bind(this);
+    }
+
+	onSubmit(event) {
+		event.preventDefault();
+
+		const url = '/rsvp';
+		let data = JSON.stringify(this.state);
+		let http = new XMLHttpRequest();
+		http.open('POST', url, true);
+
+		http.setRequestHeader('Content-type', 'application/json');
+
+		http.onreadystatechange = function() {
+			if (http.readyState == 4) {
+				console.log(http.responseText);
+				switch(http.status) {
+					case 200:
+						console.log('OK');
+						break;
+					case 400:
+						console.warn('Bad Request');
+						break;
+					case 403:
+						console.warn('Forbidden');
+						break;
+					case 418:
+						console.warn('I\'m a teapot')
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		http.send(data);
+	}
+
+	onInputChange(event) {
+		let input = event.target;
+		let name = input.name;
+		let value = input.value;
+		if (input.type === 'checkbox') {
+			value = input.checked ? value : 0;
+		}
+		this.setState({ [name]: value });
+	}
+
+	render() {
+		return (
+			<div class={style.rsvpModal + ' ' + (this.props.show ? '' : style.hide)}>
+				<h2>RSVP</h2>
+				<form onSubmit={this.onSubmit} id='rsvpForm'>
+					<div class={style.formGroup}>
+						<label for='secret'>What city did Ian and Kat move to last year?</label>
+						<input type='text' name='secret' id='secret' value={this.state.secret} onChange={this.onInputChange} />
+					</div>
+					<div class={style.formGroup}>
+						<label for='firstName'>First Name</label>
+						<input type='text' name='firstName' id='firstName' value={this.state.firstName} onChange={this.onInputChange} />
+					</div>
+					<div class={style.formGroup}>
+						<label for='lastName'>Last Name</label>
+						<input type='text' name='lastName' id='lastName' value={this.state.lastName} onChange={this.onInputChange} />
+					</div>
+					<div class={style.formGroup}>
+						<label>Will you be attending?</label>
+						<label>
+							<input type='radio' name='attending' value='yes' checked={this.state.attending === 'yes'} onChange={this.onInputChange} />
+							Attending
+						</label>
+						<label>
+							<input type='radio' name='attending' value='no' checked={this.state.attending === 'no'} onChange={this.onInputChange} />
+							Not attending
+						</label>
+					</div>
+					<div class={style.formGroup}>
+						<label>
+							<input type='checkbox' name='extras' value='1' checked={this.state.extras} onChange={this.onInputChange} />
+							Are you bringing a +1 that did not receive an invitation?
+						</label>
+					</div>
+					<button>Confirm</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export class RsvpMask extends Component {
+	render() {
+		return (
+			<div class={style.rsvpMask + ' ' + (this.props.show ? '' : style.hide)} onClick={this.props.onClick}></div>
+		);
+	}
+}
