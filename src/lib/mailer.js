e6b85f3924873bccee6eddcac10656deb9d61917
Ian Lamb <ianlamb32@gmail.com>
2017-08-12 14:39:56 -0700

    finish setting up mailer for invitations

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const nodemailer = require('nodemailer');
 
 class Mailer {
@@ -18,24 +19,27 @@ class Mailer {
      * 
      * @param {number} category - Filter mailees down by their category id. If ommitted, email all guests.
      */
-    send(category) {
+    sendInvitations(category) {
+        const dataFile = fs.readFileSync('../data.json');
+        const guestList = JSON.parse(dataFile);
+        const emailTo = guestList.guests.filter(o => o.category === category).map(o => o.email).join(', ');
+        console.log('Emailing To: %s', emailTo);
+
         // setup email data with unicode symbols
         let mailOptions = {
             from: '"Kat & Ian" <ianlamb32@gmail.com>',
-            to: '',
+            to: emailTo,
             subject: 'Kat & Ian\'s Wedding',
-            text: 'Hello world ?',
+            text: 'Test',
             html: '<b>Hello world ?</b>'
         };
 
         // send mail with defined transport object
-        this.transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message %s sent: %s', info.messageId, info.response);
-        });
+        return this.transporter.sendMail(mailOptions)
+            .then((info) => {
+                console.log('Message %s sent: %s', info.messageId, info.response);
+            });
     }
 }
 
-module.exports = Mailer;
+module.exports = new Mailer(process.env.SMTP_USER, process.env.SMTP_PASS);
